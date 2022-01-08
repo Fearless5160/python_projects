cc7ccb648946470a2c9e4ec5d6691acfec2404e4
Unmesh Zujam <88044612+Fearless5160@users.noreply.github.com>
2022-01-08 23:21:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-sub = 1
-star1 = ""
-num = int(input("Enter number of Rows: "))
-# number of Rows
-star = (2 * num) - 1
-# number of star in base rows
-for i in range(1, num + 1):
-    gap = (star - sub)/2
-    star1 = ( " " * int(gap)) + ("*" * sub)
-    sub = sub + 2
-    print(star1)
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
